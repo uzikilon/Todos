8f9743dff0de65f555851e2db72e78507dc048f8
uzikilon <ukilon@yammer-inc.com>
2012-08-16 17:03:52 -0700

    nicer build js file

--- js/build.js
+++ js/build.js
@@ -1,9 +1,9 @@
 ({
-  baseUrl: './',
-  mainConfigFile: './app.js',
+  baseUrl: '.',
+  mainConfigFile: 'app.js',
   name: '../node_modules/almond/almond',
   include: 'app',
-  out: './app.min.js',
+  out: 'app.min.js',
   logLevel: 0,
   preserveLicenseComments: false
 })
