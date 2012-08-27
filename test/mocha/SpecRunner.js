66d93f70086195f63f4ae369ef4c4d95a242f624
uzikilon <ukilon@yammer-inc.com>
2012-08-27 13:49:26 -0700

    uncomment mocha tests

--- test/mocha/SpecRunner.js
+++ test/mocha/SpecRunner.js
@@ -49,14 +49,14 @@ require(['underscore', 'jquery', 'mocha', 'chai', 'sinon'], function(_, $, mocha
 
   var specs = [];
 
-  // specs.push('spec/models/TodoSpec');
-  // specs.push('spec/views/ClearCompletedSpec');
+  specs.push('spec/models/TodoSpec');
+  specs.push('spec/views/ClearCompletedSpec');
   specs.push('spec/views/CountViewSpec');
-  // specs.push('spec/views/FooterViewSpec');
-  // specs.push('spec/views/MarkAllSpec');
-  // specs.push('spec/views/NewTaskSpec');
-  // specs.push('spec/views/TaskListSpec');
-  // specs.push('spec/views/TaskViewSpec');
+  specs.push('spec/views/FooterViewSpec');
+  specs.push('spec/views/MarkAllSpec');
+  specs.push('spec/views/NewTaskSpec');
+  specs.push('spec/views/TaskListSpec');
+  specs.push('spec/views/TaskViewSpec');
 
   require(specs, function(){
 
